c8d3754ba1977b3eb54033bb902555e8c76a5d51
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-05 19:31:22 +0000

    automatically backed up by learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,7 +1,11 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
   def change
     create_table :students do |t|
-      t.string :name
+      t.string :title
+      t.date :release_date
+      t.sting :director
+      t.string :lead
+      t.datetime :in_theaters
       end
     end
   end
